c03701fe86e5c176c16c4bd7ca119cd8a908e3aa
Frannn <francisgrabiel@gmail.com>
2025-08-30 22:58:31 -0400

    Curso java 0-1

---
+++ Basic/00_helloworld.js
@@ -0,0 +1,14 @@
+console.log("Hello, World!") // This line prints "Hello, World!" to the console
+
+console.log('Estoy empezando aprender JAVA SCRIPT 22-8-2024')/* This line prints
+"Estoy empezando aprender JAVA SCRIPT 22-8-2024"
+to the console*/
+
+console.log(`Voy a ser el mejor programador del pais 22-8-2024`)/* This line prints "Voy a ser el mejor programador del pais 22-8-2024" to the console*/
+
+
+console.log(20 + 25)
+console.log(25 - 20)
+console.log(20 * 25)
+console.log(25 / 20)
+console.log(25 % 100) 
---
+++ INVENTANDO/0-1.js
@@ -0,0 +1,12 @@
+let nota = 10
+
+if (nota >= 7) {
+    console.log("Aprovado")
+}else {
+    console.log("Reprovado")
+}
+
+if (nota = 10){
+    console.log("Aprobaste con la maxima nota")
+}
+
