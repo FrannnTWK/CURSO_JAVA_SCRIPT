56f5c7aecd3257ead3cb94dd00660bdfa73d1dfb
Frannn <francisgrabiel@gmail.com>
2025-08-30 23:41:14 -0400

    variables

--- Basic/00_helloworld.js
+++ Basic/00_helloworld.js
@@ -1,6 +1,7 @@
 console.log("Hello, World!") // This line prints "Hello, World!" to the console
 
-console.log('Estoy empezando aprender JAVA SCRIPT 22-8-2024')/* This line prints
+console.log('Estoy empezando aprender JAVA SCRIPT 22-8-2024')
+/* This line prints
 "Estoy empezando aprender JAVA SCRIPT 22-8-2024"
 to the console*/
 
@@ -12,3 +13,4 @@ console.log(25 - 20)
 console.log(20 * 25)
 console.log(25 / 20)
 console.log(25 % 100) 
+console.log(5 ** 2) //Estos dos asteriscos lo que hacen es elevaro un numero a otro osea 5 elevado a 2
---
+++ Basic/01_variables.js
@@ -0,0 +1,31 @@
+/* var --- "var" ya no es la forma debida de declarar variables 
+hoy en dia (NO DEBO HACER ESTO) */
+
+var helloworld = "Hello, JavaScript con Var"
+console.log (helloworld)
+
+helloworld = "Hola de nuevo, JavaScript con Var"
+console.log (helloworld)
+
+
+/* let --- "let" esta es la debida forma de declarar variables
+hoy en dia (DECLARAR LAS VARIABLES CON let) */
+
+let helloworld2 = "Hello, JavaScript2 con let"
+console.log (helloworld2)
+
+helloworld2 = "Hola de nuevo, JavaScript2 con let"
+console.log (helloworld2)
+
+
+//const --- "const" es una constante a la cual no le puedo cambiar su valor como he hecho con var y let.
+
+
+const helloworld3 = "Hello, JavaScript3 con const"
+console.log (helloworld3)
+
+//Esto seria un error
+
+/*
+helloworld3 = "Hola de nuevo, JavaScript3 con const"
+console.log (helloworld3)*/
